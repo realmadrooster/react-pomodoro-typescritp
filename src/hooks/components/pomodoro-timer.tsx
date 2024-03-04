58c2d28a82af0258717c3c1f551706de30ffc760
lskwr <lskwr@gmail.com>
2024-03-04 19:42:59 -0300

    typescript aula 374(configurando os audios (alarmes)

--- src/hooks/components/pomodoro-timer.tsx
+++ src/hooks/components/pomodoro-timer.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from "react";
 import { useInterval } from "../use-interval";
-import { secondsToTime } from "../../utils/seconds-to-time";
+// import { secondsToTime } from "../../utils/seconds-to-time";
 import { Button } from "./button";
 import { Timer } from "./timer";
+
+const bellStart = require('../../sounds/bell-start.mp3');
+const bellFinish = require('../../sounds/bell-finish.mp3');
+
+const audioStartWorking = new Audio(bellStart);
+const audioStopWorking = new Audio(bellFinish);
 
 interface Props {
   PomodoroTime: number;
@@ -32,6 +38,7 @@ export function PomodoroTimer(props: Props): JSX.Element {
     setWorking(true);
     setResting(false);
     setMainTime(props.PomodoroTime);
+    audioStartWorking.play();
   };
 
   const configureRest = (long: boolean) => {
@@ -44,6 +51,9 @@ export function PomodoroTimer(props: Props): JSX.Element {
     } else {
       setMainTime(props.shortRestTime);
     }
+
+    audioStopWorking.play();
+
   };
 
   return (
